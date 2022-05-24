6a1e7cefdfcaee6457772f5faa650e0289efe2fc
Schubert Rodriguez <schubert_rodriguez@crgl-thirdparty.com>
2022-05-24 18:46:23 -0500

    adds red circle to enemies for a random position on top edge

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import * as PIXI from 'pixi.js'
-//import Victor from "victor";
-//import Matter from "matter-js";
+
+import Victor from 'victor'
 
 let canvasSize = 1024
 const canvas = document.getElementById('mycanvas')
@@ -20,8 +20,30 @@ square.tint = 0xea985d
 
 app.stage.addChild(square)
 
+let enemyRadius = 32
+const enemy = new PIXI.Graphics()
+let r = randomSpawnPoint()
+enemy.position.set(r.x, r.y)
+enemy.beginFill(0xFF0000, 1)
+enemy.drawCircle(0,0, enemyRadius)
+enemy.endFill()
+app.stage.addChild(enemy)
+
 app.ticker.add(delta => {
     const cursorPosition = app.renderer.plugins.interaction.mouse.global
-    let angle = Math.atan2(cursorPosition.y - square.position.y, cursorPosition.x - square.position.x) + Math.PI / 2
+    const angle = Math.atan2(cursorPosition.y - square.position.y, cursorPosition.x - square.position.x) + Math.PI / 2
     square.rotation = angle
 })
+
+function randomSpawnPoint() {
+    const edge = 0 //Math.floor(Math.random() * 4) //Integer Random [0,3]
+    let spawnPoint = new Victor(0, 0)
+    switch (edge) {
+        case 0:
+            spawnPoint.x = canvasSize * Math.random()
+            break
+        default:
+            break
+    }
+    return spawnPoint
+}
