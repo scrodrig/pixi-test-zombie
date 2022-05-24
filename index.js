103618d2db2f66c6f8de59699c6a3a61db79a547
Schubert Rodriguez <schubert_rodriguez@crgl-thirdparty.com>
2022-05-24 18:30:36 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import * as PIXI from 'pixi.js'
+//import Victor from "victor";
+//import Matter from "matter-js";
+
+let canvasSize = 1024
+const canvas = document.getElementById('mycanvas')
+const app = new PIXI.Application({
+    view: canvas,
+    width: canvasSize,
+    height: canvasSize,
+    backgroundColor: 0x5c812f
+})
+
+let squareWidth = 64
+const square = new PIXI.Sprite(PIXI.Texture.WHITE)
+square.anchor.set(0.5)
+square.position.set(app.screen.width / 2, app.screen.height / 2)
+square.width = square.height = squareWidth
+square.tint = 0xea985d
+
+app.stage.addChild(square)
+
